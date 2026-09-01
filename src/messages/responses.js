91061518265b2e697540f16069662dd0a0c443cb
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-09-01 17:17:14 +0330

    Create responses.js

---
+++ src/messages/responses.js
@@ -0,0 +1,27 @@
+export const responses = {
+  address: `
+آدرس ما:
+
+تهران، خیابان مثال، پلاک ۱۲
+
+لوکیشن:
+https://maps.google.com/
+`,
+
+  price: `
+قیمت محصول:
+
+200,000 تومان
+`,
+
+  contact: `
+برای ارتباط با پشتیبانی:
+
+@YourUsername
+`,
+
+  hello: `
+سلام.
+چطور می‌تونم کمکت کنم؟
+`
+};
