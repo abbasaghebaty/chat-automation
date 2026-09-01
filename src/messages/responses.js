ddfb738d402232d8e768685b440d5796ba53a462
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-09-01 17:48:27 +0330

    Update responses.js

--- src/messages/responses.js
+++ src/messages/responses.js
@@ -5,12 +5,7 @@ export const responses = {
     latitude: 36.325273576311425,
     longitude: 59.60890905931591,
 
-    text: `
-مغازه ما بین هنرور ۲۰ و ۲۲ قرار دارد.
-
-کانال ایتا و روبیکا:
-@Shoma_shop
-`,
+    text: "مغازه ما بین هنرور ۲۰ و ۲۲ قرار دارد.\n\nکانال ایتا و روبیکا:\n@Shoma_shop",
 
     buttons: [
       {
@@ -26,25 +21,16 @@ export const responses = {
 
   price: {
     type: "text",
-    text: `
-جهت مشاهده کامل محصولات و قیمت ها ، لطفا عضو کانال ما در ایتا یا روبیکا شوید :
-@shoma_shop
+    text: "جهت مشاهده کامل محصولات و قیمت ها، لطفا عضو کانال ما در ایتا یا روبیکا شوید:\n@shoma_shop"
   },
 
   support: {
     type: "text",
-    text: `
-برای ارتباط با پشتیبانی:
-
-@Shoma_shop_sup
-`
+    text: "برای ارتباط با پشتیبانی:\n\n@Shoma_shop_sup"
   },
 
   hello: {
     type: "text",
-    text: `
-سلام.
-جانم درخدمتم
-`
+    text: "سلام.\nجانم درخدمتم"
   }
 };
