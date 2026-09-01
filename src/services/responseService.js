ed975290f7b9c063ac57fb79f90c744293e11d86
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-09-01 17:48:15 +0330

    Update responseService.js

--- src/services/responseService.js
+++ src/services/responseService.js
@@ -1,3 +1,4 @@
+import { InlineKeyboard } from "grammy";
 import { responses } from "../messages/responses.js";
 
 export async function sendResponse(ctx, automation) {
@@ -8,5 +9,32 @@ export async function sendResponse(ctx, automation) {
     return;
   }
 
-  await ctx.reply(response);
+  if (response.type === "text") {
+    await ctx.reply(response.text);
+    return;
+  }
+
+  if (response.type === "location") {
+    const keyboard = new InlineKeyboard();
+
+    for (const button of response.buttons ?? []) {
+      keyboard.url(button.text, button.url);
+    }
+
+    await ctx.replyWithLocation(
+      response.latitude,
+      response.longitude,
+      {
+        reply_markup: keyboard
+      }
+    );
+
+    if (response.text) {
+      await ctx.reply(response.text);
+    }
+
+    return;
+  }
+
+  console.error(`Unknown response type: ${response.type}`);
 }
