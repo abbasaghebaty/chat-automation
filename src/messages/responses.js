4ba1e965bd3e3d20e73b4e7a51e1e6b9bbbab549
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-09-01 18:03:30 +0330

    Update responses.js

--- src/messages/responses.js
+++ src/messages/responses.js
@@ -5,11 +5,7 @@ export const responses = {
     latitude: 36.325273576311425,
     longitude: 59.60890905931591,
 
-    caption: `
-مغازه ما بین هنرور ۲۰ و ۲۲ قرار دارد.
-
-برای مسیریابی از دکمه‌های زیر استفاده کنید.
-`,
+    text: "مغازه ما بین هنرور ۲۰ و ۲۲ قرار دارد.",
 
     buttons: [
       [
@@ -27,12 +23,10 @@ export const responses = {
     ]
   },
 
-
   hello: {
     type: "text",
     text: "سلام.\nجانم در خدمتم"
   },
-
 
   support: {
     type: "text",
