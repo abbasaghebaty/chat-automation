dc2858a703122d0a9cb39dfa73679ccb5104a106
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-09-01 17:53:39 +0330

    Update responses.js

--- src/messages/responses.js
+++ src/messages/responses.js
@@ -1,24 +1,25 @@
 export const responses = {
-  address: {
-    type: "location",
+ address: {
+  type: "location",
 
-    latitude: 36.325273576311425,
-    longitude: 59.60890905931591,
+  latitude: 36.325273576311425,
+  longitude: 59.60890905931591,
 
-    text: "مغازه ما بین هنرور ۲۰ و ۲۲ قرار دارد.\n\nکانال ایتا و روبیکا:\n@Shoma_shop",
+  text: "مغازه ما بین هنرور ۲۰ و ۲۲ قرار دارد.",
 
-    buttons: [
-      {
-        text: "نشان",
-        url: "https://nshn.ir/35Qb1MaUIJjDVc"
-      },
-      {
-        text: "Google Maps",
-        url: "https://maps.app.goo.gl/Haixv2k28U9JJi878"
-      }
-    ]
-  },
-
+  buttons: [
+    {
+      text: "نشان",
+      url: "https://nshn.ir/35Qb1MaUIJjDVc",
+      style: "primary"
+    },
+    {
+      text: "Google Maps",
+      url: "https://maps.app.goo.gl/Haixv2k28U9JJi878",
+      style: "primary"
+    }
+  ]
+}
   price: {
     type: "text",
     text: "جهت مشاهده کامل محصولات و قیمت ها، لطفا عضو کانال ما در ایتا یا روبیکا شوید:\n@shoma_shop"
