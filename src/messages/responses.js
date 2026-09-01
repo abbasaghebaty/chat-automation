19ca5e6d3acbbf57a54b129806771ea9b343342c
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-09-01 17:55:16 +0330

    Update responses.js

--- src/messages/responses.js
+++ src/messages/responses.js
@@ -1,37 +1,41 @@
 export const responses = {
- address: {
-  type: "location",
+  address: {
+    type: "location",
 
-  latitude: 36.325273576311425,
-  longitude: 59.60890905931591,
+    latitude: 36.325273576311425,
+    longitude: 59.60890905931591,
 
-  text: "مغازه ما بین هنرور ۲۰ و ۲۲ قرار دارد.",
+    caption: `
+مغازه ما بین هنرور ۲۰ و ۲۲ قرار دارد.
 
-  buttons: [
-    {
-      text: "نشان",
-      url: "https://nshn.ir/35Qb1MaUIJjDVc",
-      style: "primary"
-    },
-    {
-      text: "Google Maps",
-      url: "https://maps.app.goo.gl/Haixv2k28U9JJi878",
-      style: "primary"
-    }
-  ]
-}
-  price: {
-    type: "text",
-    text: "جهت مشاهده کامل محصولات و قیمت ها، لطفا عضو کانال ما در ایتا یا روبیکا شوید:\n@shoma_shop"
+برای مسیریابی از دکمه‌های زیر استفاده کنید.
+`,
+
+    buttons: [
+      [
+        {
+          text: "نشان",
+          url: "https://nshn.ir/35Qb1MaUIJjDVc",
+          style: "primary"
+        },
+        {
+          text: "Google Maps",
+          url: "https://maps.app.goo.gl/Haixv2k28U9JJi878",
+          style: "primary"
+        }
+      ]
+    ]
   },
 
-  support: {
-    type: "text",
-    text: "برای ارتباط با پشتیبانی:\n\n@Shoma_shop_sup"
-  },
 
   hello: {
     type: "text",
-    text: "سلام.\nجانم درخدمتم"
+    text: "سلام.\nجانم در خدمتم"
+  },
+
+
+  support: {
+    type: "text",
+    text: "برای ارتباط با پشتیبانی پیام دهید."
   }
 };
