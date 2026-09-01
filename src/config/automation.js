526d9cb6b7dedd7d31cb2483cd88d4cf3b6301ee
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-09-01 17:18:02 +0330

    Create automation.js

---
+++ src/config/automation.js
@@ -0,0 +1,44 @@
+export const automations = [
+  {
+    name: "address",
+    keywords: [
+      "آدرس",
+      "ادرس",
+      "آدرس کجاست",
+      "ادرس کجاست"
+    ],
+    response: "address"
+  },
+
+  {
+    name: "price",
+    keywords: [
+      "قیمت",
+      "قیمتش",
+      "چنده",
+      "قیمت محصول"
+    ],
+    response: "price"
+  },
+
+  {
+    name: "contact",
+    keywords: [
+      "پشتیبانی",
+      "تماس",
+      "ارتباط",
+      "ادمین"
+    ],
+    response: "contact"
+  },
+
+  {
+    name: "hello",
+    keywords: [
+      "سلام",
+      "درود",
+      "های"
+    ],
+    response: "hello"
+  }
+];
