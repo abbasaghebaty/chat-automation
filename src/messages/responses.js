d11db15a7511523da0916eff99891e2278900c6c
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-09-03 18:15:22 +0330

    Update responses.js

--- src/messages/responses.js
+++ src/messages/responses.js
@@ -67,13 +67,39 @@ export const responses = {
     ]
   },
 
-  price: {
-    type: "text",
+price: {
+  type: "text",
 
-    text:
-      "اسم محصول رو بفرستید تا قیمتش رو اعلام کنیم."
-  },
+  text:
+    "همه اجناس و قیمت‌های شما شاپ در کانال‌ها قرار گرفته‌اند و می‌تونید لیست محصولات و قیمت‌ها رو اونجا ببینید.\n\n" +
+    "با این حال، اگه مایل هستین، اسم محصول رو همین‌جا بفرستین تا قیمتش رو بهتون اعلام کنیم.",
 
+  buttons: [
+    [
+      {
+        text:
+          "ایتا",
+
+        url:
+          "https://eitaa.com/shoma_shop",
+
+        style:
+          "primary"
+      },
+
+      {
+        text:
+          "روبیکا",
+
+        url:
+          "https://rubika.ir/shoma_shop",
+
+        style:
+          "primary"
+      }
+    ]
+  ]
+},
   productSearch: {
     type: "text",
 
